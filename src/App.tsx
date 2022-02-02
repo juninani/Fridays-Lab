5fb82b3205892096b4a564586fa13f0ed4766ffa
hyeongyu9706 <54524@naver.com>
2022-02-02 09:41:43 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+export default function App() {
+  return <div>React + TypeScript + Webpack!</div>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+    ],
+  },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          templateParameters: {
+            env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
+            },
+            filename: "./index.html",
+            favicon: './public/favicon.ico',
+          minify:
+            process.env.NODE_ENV === 'production'
+              ? {
+                  collapseWhitespace: true,
+                  removeComments: true,
+                }
+              : false,
+        }),
+        new CleanWebpackPlugin(),
+      ],
+};
