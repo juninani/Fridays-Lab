9c73cd2f42df98b862ebf9dcaf1f57b138f97ee5
leejunhee <juninani123@gmail.com>
2022-04-06 22:48:36 +0900

    폴더추가

---
+++ src/Components/Common/CompExam/componentsExample.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./componentsExample.scss"
+const index = () => {
+    return (
+        <div >
+            <header>
+                <h1>ComponentsExam</h1>
+            </header>
+        </div>
+    );
+};
+
+export default index;
