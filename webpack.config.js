f92c204262b14858b43b39f97f154e620be660da
hyeongyu9706 <54524@naver.com>
2022-04-13 13:21:00 +0900

    절대경로 지정

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,63 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const  TsconfigPathsPlugin  =  require('tsconfig-paths-webpack-plugin') ;
 const mode = process.env.NODE_ENV || 'development';
-
+const tsConfigPath = path.resolve(__dirname, "./tsconfig.json") 
 module.exports = {
-  mode,
-  entry: {
-    app: path.join(__dirname, 'src', 'index.tsx'),
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-          },
-          {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-              },
-            ],
-          },
-    ],
-  },
+    mode,
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx'),
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        plugins: [new TsconfigPathsPlugin({ configFile: tsConfigPath })],
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
-          template: './public/index.html',
-          templateParameters: {
-            env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
+            template: './public/index.html',
+            templateParameters: {
+                env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
             },
-            filename: "./index.html",
+            filename: './index.html',
             favicon: './public/favicon.ico',
-          minify:
-            process.env.NODE_ENV === 'production'
-              ? {
-                  collapseWhitespace: true,
-                  removeComments: true,
-                }
-              : false,
+            minify:
+                process.env.NODE_ENV === 'production'
+                    ? {
+                          collapseWhitespace: true,
+                          removeComments: true,
+                      }
+                    : false,
         }),
         new CleanWebpackPlugin(),
-      ],
+    ],
 };
