dc303c795f39b17346bdea5c1dc279b682944b25
leejunhee <juninani123@gmail.com>
2022-04-14 17:54:13 +0900

    stroybook 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
-module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-    },
-    extends: ['naver', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: ['@typescript-eslint/parser'],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint', 'prettier', 'eslint-plugin-prettier', 'eslint-config-prettier'],
-    rules: {
-        indent: ['error', 'tab'],
-        'linebreak-style': ['error', 'windows'],
-        quotes: ['error', 'double'],
-        semi: ['error', 'always'],
-    },
-};
+// module.exports = {
+//     env: {
+//         browser: true,
+//         es6: true,
+//     },
+//     extends: ['naver', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+//     parser: ['@typescript-eslint/parser'],
+//     parserOptions: {
+//         ecmaFeatures: {
+//             jsx: true,
+//             tsx: true,
+//         },
+//         ecmaVersion: 'latest',
+//         sourceType: 'module',
+//     },
+//     plugins: ['react', '@typescript-eslint', 'prettier', 'eslint-plugin-prettier', 'eslint-config-prettier'],
+//     rules: {
+//         indent: ['error', 'tab'],
+//         'linebreak-style': ['error', 'windows'],
+//         quotes: ['error', 'double'],
+//         semi: ['error', 'always'],
+//     },
+// };
 
 // module.exports = {
 //     env: {
